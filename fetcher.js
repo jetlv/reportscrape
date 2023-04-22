c00028cb36664d78d5e62f378f75e9286ba3a5dd
Lvchao <jetlyu@aliyun.com>
2023-04-22 21:29:19 +0800

    move to bright data

--- fetcher.js
+++ fetcher.js
@@ -32,10 +32,16 @@ const proxies = [`us-wa.proxymesh.com:31280`, `us-il.proxymesh.com:31280`, `us.p
 
 function grabAnProxyServer() {
     let random = Math.floor(Math.random() * proxies.length);
-    let proxy = `http://${proxies[random]}`
+    let proxy = `http://${proxies[random]}`    
     return proxy
+    //return "http://zproxy.lum-superproxy.io:22225"
 }
 
+function grabAnProxyServerBrightData() { 
+    return "http://zproxy.lum-superproxy.io:22225"
+}
+
+// For proxy mesh
 async function subQuerySteps(page, rdNumber, date) {
     await page.goto(`https://crash.chicagopolice.org/DriverInformationExchange/home`);      
     await page.waitForSelector(`#rd`, {visible: true, timeout: 30000})                 
@@ -54,6 +60,31 @@ async function subQuerySteps(page, rdNumber, date) {
     return postStatusCode
 }
 
+
+// For bright data
+async function subQueryStepsBrightData(page, rdNumber, date) {
+    await page.goto(`https://crash.chicagopolice.org/DriverInformationExchange/home`);      
+    await page.waitForSelector(`#rd`, {visible: true, timeout: 30000})                 
+    await page.type('#rd', rdNumber); // 替换选择器和实际的RD Number
+    await page.type('#crashDate', date); // 替换选择器和实际的Date    
+
+    //console.log(`Get response ${recaptchaResponse}`)
+    await page.evaluate(() => {
+        const recaptchaInput = document.createElement('input');
+        recaptchaInput.setAttribute('type', 'hidden');
+        recaptchaInput.setAttribute('id', 'g-recaptcha-response');
+        recaptchaInput.setAttribute('name', 'g-recaptcha-response');
+        recaptchaInput.value = "03AKH6MRExXBDD_EXn6WATftWACnoAh7bMpKV3LzLIkbCzuy2Lh1ta3FxHgQrfeUbeZFmlTkZiLz1fqikH3fe6ZRLwhxxne2Qri_ZjRblPKSEVFthPlvxf65vlPJnWR9Z774B86De7gWf8x6OoxWukS_szWqcoVUz9AriO6S7xSjNcLHklHIhfLFFzlz0YSiorN7TDD3xFFZAe5D_07ENM3rAm02NyNGIcQyG3LaWKLZ4aX0H2qf2Tp2Zm69iedpmaQQ1zkW-P1cJhuoAiZ84Vd7dVYW3XP-3_WUtZ2ISN2ySr8atKJsJz_MLbQN_ELfhAdwvkHzwVVK6QN9QTv-NvLesW_iA0rpDMaa_h-gh2J9T74SlaXOP0cCghj0A1LRmB5V1d79nXht7QqKbVf-5F8zyy6xBGvVWNZauEaRzECz4tHoEZ8SN1rYL1fF0cyAjDsfnzrVAfuia7chR3ErDYesjlGrdh8MFdkW-Rzb4-5IgjW8r_CChiegt5VcaNBBns2gbKtyBvKykkdXylR3A-1k210pTwhzeBdgIlwl6ed2ULEmq6oQGHLl0-rJTNnRiUpsHvcRY-KsuOwU2mhuHF_KbTgZK1G6KA1y64ygdjBTq3EU1BO7a9rKs";
+        document.querySelector('form').appendChild(recaptchaInput);
+    });
+
+    const navigationPromise = page.waitForNavigation();
+    await page.click('input[value="Submit"]'); // 替换为实际的提交按钮选择器
+    const postResponse = await navigationPromise;
+    const postStatusCode = postResponse.status();
+    return postStatusCode
+}
+
 async function getDriverInformationExchangeResponse(rdNumber, date) {
     let browser = null
     let html = ''
@@ -62,13 +93,16 @@ async function getDriverInformationExchangeResponse(rdNumber, date) {
     try {     
         browser = await puppeteer.launch({
             // args: ['--no-sandbox', `--proxy-server=http://127.0.0.1:1080`],
-            args:  ['--no-sandbox', '--disable-setuid-sandbox',`--proxy-server=${grabAnProxyServer()}`],
+            args:  ['--no-sandbox', '--disable-setuid-sandbox',`--proxy-server=${grabAnProxyServerBrightData()}`],
             headless: true,
             ignoreHTTPSErrors: true,
             userDataDir: tmpFolder.name
         });                                
         const page = await browser.newPage();    
-        await page.authenticate({username: 'emckibbin@accident-data.com', password: 'UzT8gAYJHixPa8S'})
+        // Bright data
+        await page.authenticate({username: 'brd-customer-hl_60aa431d-zone-zone1', password: 'lkl8tbg6up2o'})
+        // Proxy mesh
+        //await page.authenticate({username: 'emckibbin@accident-data.com', password: 'UzT8gAYJHixPa8S'})
         page.on('response', async (response) => {
             // console.log(`URL: ${response.url()}`);
             if(response.url() == 'https://crash.chicagopolice.org/DriverInformationExchange/home'){
@@ -83,7 +117,7 @@ async function getDriverInformationExchangeResponse(rdNumber, date) {
             }
             });
         let postGot200 = false        
-        let postStatusCode = await subQuerySteps(page, rdNumber, date)
+        let postStatusCode = await subQueryStepsBrightData(page, rdNumber, date)
         if(postStatusCode == 200) {
             postGot200 = true
         }            
@@ -91,7 +125,7 @@ async function getDriverInformationExchangeResponse(rdNumber, date) {
             console.log(`${rdNumber} + ${date} Blocked by cloudflare 429, we need to hard delay 60s and retry`)                
             await Promise.delay(60000)
             console.log(`${rdNumber} + ${date} retrying`)                
-            postStatusCode = await subQuerySteps(page, rdNumber, date)
+            postStatusCode = await subQueryStepsBrightData(page, rdNumber, date)
             if(postStatusCode == 429) {
                 console.log(`${rdNumber} + ${date} got 429 again after retry, end process`)                
             }
